544af2f1e011791dab99550ec183ad833b2d6476
ljurk <ljurk@pm.me>
2019-01-12 17:44:15 +0100

    add subsites

--- rest/api.py
+++ rest/api.py
@@ -9,6 +9,7 @@ import psycopg2
 app = Flask(__name__)
 api = Api(app)
 app.config['JSON_SORT_KEYS'] = False
+host = 'http://localhost:5001/'
 
 #initiate parser for put
 parser = reqparse.RequestParser()
@@ -21,7 +22,7 @@ con = psycopg2.connect( "dbname='partman' user='partman' host='partmandb' passwo
 cur = con.cursor()
 output =""
 
-class Product(Resource):
+class Parts(Resource):
     def getParts(self):
         cur.execute("SELECT * FROM parts;")
         output = cur.fetchone()
@@ -48,8 +49,53 @@ class Product(Resource):
         part = {'id': output[0],'categoryId': output[1], 'name': output[2], 'friendlyName': output[3] }
         return part, 201
 
+
+class Categories(Resource):
+    def getCategories(self):
+        cur.execute("SELECT * FROM categories;")
+        output = cur.fetchone()
+        print(output[0])
+        cats=[]
+        while output != None:
+            cat = {'id': output[0],'name': output[1] }
+            cats.append(cat)
+            output = cur.fetchone()
+        return cats
+
+    def get(self):
+        return(jsonify({'categories':self.getCategories()}))
+
+    def put(self):
+        args = parser.parse_args()
+        sqlCommand = "INSERT INTO categories(name) VALUES('" + args['name'] + "')"
+
+        cur.execute(sqlCommand)
+        con.commit()
+        #read new entry
+        cur.execute("SELECT * FROM categories ORDER BY id DESC LIMIT 1;")
+        output = cur.fetchone()
+        part = {'id': output[0],'name': output[1]}
+        return part, 201
+
+class index(Resource):
+    def getCategories(self):
+        cur.execute("SELECT tablename FROM pg_catalog.pg_tables WHERE tableowner = 'partman';")
+        output = cur.fetchone()
+        print(output[0])
+        cats=[]
+        while output != None:
+            cat = {'path': host + output[0]}
+            cats.append(cat)
+            output = cur.fetchone()
+        return cats
+
+    def get(self):
+        return(jsonify({'categories':self.getCategories()}))
+
 # Create routes
-api.add_resource(Product, '/')
+api.add_resource(index, '/')
+api.add_resource(Parts, '/parts')
+api.add_resource(Categories, '/categories')
 
 # Run the application
 if __name__ == '__main__':
