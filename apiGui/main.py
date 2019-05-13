eadeb9e1d1f51d483368041819bd2cafbd4e272c
ljurk <ljurk@pm.me>
2019-05-13 20:15:02 +0200

    better split between input and output, but still not the yellow from the egg

--- apiGui/main.py
+++ apiGui/main.py
@@ -6,63 +6,61 @@ host = 'http://localhost:5001/parts'
 class apiGui:
     app = QApplication([])
     mainWindow = QMainWindow()
-    window = QWidget()
-    addwindow = QWidget()
-    table = QTableWidget(0, 0)
     tabWidget = QTabWidget()
-    heading = table.horizontalHeader()
-    addlayout = QVBoxLayout()
-    debugWindow = QTextEdit()
-    debugWindow.setReadOnly(True)
+    #output
+    oWindow = QWidget()
+    oTable = QTableWidget(0, 0)
+    heading = oTable.horizontalHeader()
+    oDebug = QTextEdit()
+    oDebug.setReadOnly(True)
+    oUrl = QLineEdit(host)
+    #input
+    iWindow = QWidget()
+    iLayout = QVBoxLayout()
+    iDebug = QTextEdit()
+    iDebug.setReadOnly(True)
+    iTable = QTableWidget(0, 0)
     def show(self):
         self.mainWindow.setWindowTitle("MAIN")
 
-        #main
-        layout = QVBoxLayout()
-        layout.addWidget(QLabel("URL"))
-        mainURL = QLineEdit(host)
-        layout.addWidget(mainURL)
-        layout.addWidget(self.table)
-        btnReload = QPushButton('reload')
-        btnReload.clicked.connect(lambda:self.fillTable(mainURL.text()))
-        layout.addWidget(btnReload)
-        self.debugWindow.setText("show success")
-        self.debugWindow.append("show success")
-        self.debugWindow.show()
-        layout.addWidget(self.debugWindow)
+        self.showOutput()
+        self.showInput()
 
-        self.window.setLayout(layout)
-
-        #add
-        self.addlayout.addWidget(QLabel("URL"))
-        addURL = QLineEdit(host)
-        self.addlayout.addWidget(addURL)
-        btnSend = QPushButton('send')
-        btnSend.clicked.connect(lambda:self.send(addURL.text()))
-        self.addlayout.addWidget(btnSend)
-        self.addwindow.setLayout(self.addlayout)
-
-        self.tabWidget.addTab(self.window, "main")
-        self.tabWidget.addTab(self.addwindow, "add")
-        self.tabWidget.currentChanged.connect(lambda:self.reloadInputs(mainURL.text()))
+        self.tabWidget.addTab(self.oWindow, "main")
+        self.tabWidget.addTab(self.iWindow, "add")
+        #self.tabWidget.currentChanged.connect(lambda:self.reloadInputs(self.oUrl.text()))
         self.tabWidget.show()
         self.app.exec_()
 
+    def showOutput(self):
+        layout = QVBoxLayout()
+        layout.addWidget(QLabel("URL"))
+        layout.addWidget(self.oUrl)
+        layout.addWidget(self.oTable)
+        btnReload = QPushButton('reload')
+        btnReload.clicked.connect(lambda:self.fillTable(self.oUrl.text()))
+        layout.addWidget(btnReload)
+        layout.addWidget(self.oDebug)
+
+        self.oWindow.setLayout(layout)
+    def showInput(self):
+        self.iLayout.addWidget(QLabel("URL"))
+        addURL = QLineEdit(host)
+        self.iLayout.addWidget(addURL)
+        btnSend = QPushButton('send')
+        btnSend.clicked.connect(lambda:self.send(addURL.text()))
+        self.iLayout.addWidget(btnSend)
+        self.iLayout.addWidget(self.iDebug)
+        self.iWindow.setLayout(self.iLayout)
+
     def log(self, text):
-        self.debugWindow.append(text)
-        self.debugWindow.show()
+        self.oDebug.append(text)
+        self.oDebug.show()
+        self.iDebug.append(text)
+        self.iDebug.show()
 
-    def clearLayout(self, layout):
-        for i in reversed(range(layout.count())):
-            layout.itemAt(i).widget().setParent(None)
-        return layout
-
-    def fillTable(self, url):
-        self.table.setRowCount(0)
-        self.table.setColumnCount(0)
-        self.table.clear()
-        print(url)
-        data=0
+    def request(self, url):
+        data = 0
         try:
             #data = requests.get(url).json()
             r = requests.get(url)
@@ -76,7 +74,19 @@ class apiGui:
             self.log("Timeout Error:"+str(errt))
         except requests.exceptions.RequestException as err:
             self.log("OOps: Something Else"+str(err))
+        return data
 
+    def clearLayout(self, layout):
+        for i in reversed(range(layout.count())):
+            layout.itemAt(i).widget().setParent(None)
+        return layout
+
+    def fillTable(self, url):
+        self.oTable.setRowCount(0)
+        self.oTable.setColumnCount(0)
+        self.oTable.clear()
+        print(url)
+        data = self.request(url)
         if data != 0:
             self.log("request ok")
             row = 0
@@ -84,30 +94,30 @@ class apiGui:
             header = list(list(data.values())[0][0].keys())
             for d in data[key]:
                 col = 0
-                self.table.setRowCount(row + 1)
+                self.oTable.setRowCount(row + 1)
                 for k in d.keys():
                     if row == 0:
-                        self.table.setColumnCount(col + 1)
+                        self.oTable.setColumnCount(col + 1)
                         self.heading.setSectionResizeMode(col, QHeaderView.Stretch)
                         # or self.heading.setSectionResizeMode(col, QHeaderView.ResizeToContents)
-                    self.table.setItem(row, col, QTableWidgetItem(d[k]))
+                    self.oTable.setItem(row, col, QTableWidgetItem(d[k]))
                     col += 1
                 row+=1
-            self.table.setHorizontalHeaderLabels(header)
+            self.oTable.setHorizontalHeaderLabels(header)
 
 
     def reloadInputs(self, url):
-        #self.addlayout = QVBoxLayout()
-        self.addlayout = self.clearLayout(self.addlayout)
+        #self.iLayout = QVBoxLayout()
+        self.iLayout = self.clearLayout(self.iLayout)
         data = requests.get(url).json()
         row = 0
         key = list(data.keys())[0]
         header = list(list(data.values())[0][0].keys())
         for t in header:
-            self.addlayout.addWidget(QLabel(t))
-            self.addlayout.addWidget(QLineEdit())
+            self.iLayout.addWidget(QLabel(t))
+            self.iLayout.addWidget(QLineEdit())
 
-        self.addwindow.setLayout(self.addlayout)
+        self.iWindow.setLayout(self.iLayout)
 
     def send(self, url):
         print(url)
